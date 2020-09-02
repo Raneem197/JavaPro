d5b6cc064e68e038cfc37d4c80941e72edc36eba
Raneem197 <Raneemalotaibi97@gmail.com>
2020-09-02 21:27:20 +0300

    Register&Login_activities

---
+++ app/src/main/java/com/raneem/javapro/MainActivity.java
@@ -0,0 +1,15 @@
+package com.raneem.javapro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
