044e4af34141e6a84274b71e4981e7941cf9005a
Raneem197 <Raneemalotaibi97@gmail.com>
2020-09-17 20:08:30 +0300

    Register&Login_activities

--- app/src/main/java/com/raneem/javapro/LogIn.java
+++ app/src/main/java/com/raneem/javapro/LogIn.java
@@ -10,5 +10,8 @@ public class LogIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+
+
     }
 }
--- app/src/main/java/com/raneem/javapro/MainActivity.java
+++ app/src/main/java/com/raneem/javapro/MainActivity.java
@@ -1,15 +1,71 @@
 package com.raneem.javapro;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    EditText username, email, pass;
+    Button regester;
+
+    FirebaseAuth mFirebaseAuth;
+    FirebaseDatabase firebaseDatabase;
+
+   
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = (EditText) findViewById(R.id.editUsername);
+        email = (EditText) findViewById(R.id.editEmail);
+        pass = (EditText) findViewById(R.id.editPassword);
+        regester = (Button) findViewById(R.id.button);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+
+    }
+
+    public void Register(View view){
+
+        String email_ll=email.getText().toString();
+        String password=pass.getText().toString();
+
+        mFirebaseAuth.createUserWithEmailAndPassword(email_ll, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("my_stor", "createUserWithEmail:success");
+                            FirebaseUser user = mFirebaseAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("my_stor", "createUserWithEmail:failure", task.getException());
+
+
+                        }
+
+                    }
+                });
     }
 }
+
 
